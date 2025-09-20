df381f25667cd4b71c59c80666eec5921d1724dc
raj-dev-1-git <raj.salunke.dev@gmail.com>
2025-09-21 03:57:39 +0530

    Initial commit

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="neu",
+    name="n-rex",
     version="0.1.0",
     packages=find_packages(),
     install_requires=["numpy"],
-    description="neu: Load, predict, repeat.",
+    description="n-rex: Load, predict, repeat.",
     author="Raj",
     url="https://github.com/raj-dev-1-git/n-rex",
     long_description=open("README.md").read(),
